016b5e68bdd9c3b8175bb9fea1a4f1375c046a15
at0m1ccc <127032639+at0m1ccc@users.noreply.github.com>
2024-02-23 16:51:00 +0500

    Add files via upload

---
+++ src/main/java/org/example/skb/SkbApplication.java
@@ -0,0 +1,13 @@
+package org.example.skb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/skb/SkbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.skb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
